484e5daff08ae11cee7d5c205efae8fcb46a8f46
Nielvid <56310246+nielvid@users.noreply.github.com>
2022-03-17 14:20:40 +0100

    upload with pre-signed url working as as this commit it

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPage = () => {
 
   const [image, setImage] = useState({ preview: '', data: '' })
   const [data, setData] = useState({ name: '', type: '' })
+   const [url, setUrl] = useState('')
   const [status, setStatus] = useState('')
   const handleSubmit = async (e:any) => {
     e.preventDefault()
@@ -18,7 +19,7 @@ const Home: NextPage = () => {
    
       console.log(data, "name and type")
       
-    const response = await axios.post ('http://localhost:3010/utility/get-signed-url', data, {
+    const response = await axios.post('http://localhost:3010/utility/get-signed-url', data, {
       headers:{
         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InR5ZUBnbWFpbC5jb20iLCJ1c2VySWQiOiIzMjFjYjc1MC1iYTdkLTRlYjgtODZkNS1iYTIyMzBjMWNhNjYiLCJyb2xlIjoiY2xpZW50IiwiZmlyc3ROYW1lIjoiWWVzamoiLCJsYXN0bmFtZSI6Ikdpc3N5aGgiLCJpYXQiOjE2NDU1MzUzMjcsImV4cCI6MTY1MDcxOTMyN30.RHfuJEmd6nlZPq9jvteIt4THYwCJablnSbomYEzR_Ec`
       }
@@ -26,11 +27,14 @@ const Home: NextPage = () => {
     if (response) setStatus(response.statusText)
   
        console.log(response, "from server")
-       console.log(response.data.data.data, 'url to upload to  aws s3')
- const toS3 = await axios.put(`https://cors-anywhere.herokuapp.com/${response.data.data.data}`, formData)
-    console.log(toS3, 'response from aws s3')
+       console.log(response.data.data, 'url to upload to  aws s3')
 
+      //  setUrl(response.data.data.data.url)
+ 
+const toS3 = await axios.put(`${response.data.data.data}`, image.data)
+    console.log(toS3, 'response from aws s3')
   }
+  
 
   const handleFileChange = (e:any) => {
     const img = {
@@ -68,6 +72,9 @@ const Home: NextPage = () => {
       </form>
       {status && <h4>{status}</h4>}
     </div>
+    {/* <div>
+      <img src={url} alt="image from aws" />
+    </div> */}
       </main>
 
      
